5a7bdeb153aa9bf2da5fe281bc892bd95a7af783
Paul Ferguson <paullferguson@gmail.com>
2020-06-08 15:55:29 -0700

    Quick fix to logo and page layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <h4>Login with Github and get hand documentation for your project.</h4>
-    <a href="/auth/github">Login With Github</a>
+    <img alt="duck logo" src="../assets/logo.png">
+    <h4>Login with Github and get handys documentation for your project.</h4>
+    <a class="btn" href="/auth/github">Login With Github</a>
   </div>
 </template>
 
@@ -15,3 +15,28 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+
+  h4 {
+    padding: 16px;
+  }
+
+  .btn {
+    text-decoration: none;
+    border-radius: 4px;
+    border: solid 1px black;
+    padding: 8px 32px;
+    font-size: 18px;
+    color: black;
+    display: inline-block;
+    line-height: 2;
+    font-weight: bold;
+    transition: all 250ms ease;
+
+    &:hover {
+      background-color: rgb(253, 213, 193);
+    }
+  }
+
+</style>
